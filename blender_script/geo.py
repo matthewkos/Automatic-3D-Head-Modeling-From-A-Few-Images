1dbb22c1a335cbbd658d80a42b03e94b002550a3
Ken Cheung <ykcheungab@connect.ust.hk>
2019-03-21 15:34:35 +0800

    modified script to read ear indices and then delete

--- blender_script/geo.py
+++ blender_script/geo.py
@@ -420,7 +420,7 @@ class HeadMask_Align:
         jaw_ind = np.loadtxt(os.path.join(DIR_KPTS, 'jaw.txt')).astype(np.int32)
         neck_ind = np.loadtxt(os.path.join(DIR_KPTS, 'neck.txt')).astype(np.int32)
         ear_ind = np.loadtxt(os.path.join(DIR_KPTS, 'ear.txt')).astype(np.int32)
-        return kpt_ind, left_ind, fore_ind, jaw_ind, ind_bound, neck_ind
+        return kpt_ind, left_ind, fore_ind, jaw_ind, ind_bound, neck_ind, ear_ind
 
     def get_scale(self, face, head):
         P1_REF = 52447
@@ -520,7 +520,7 @@ class HeadMask_Align:
         """
         GET data from files
         """
-        kpt_ind, left_ind, fore_ind, jaw_ind, ind_bound, neck_ind = self.get_kpts()
+        kpt_ind, left_ind, fore_ind, jaw_ind, ind_bound, neck_ind, ear_ind = self.get_kpts()
         object_mode('Head')
         import_obj(os.path.join(DIR_MASK, MASK_DATA), 'Object')
         bpy.context.scene.objects.active = None
@@ -552,6 +552,7 @@ class HeadMask_Align:
 
         bpy.ops.mesh.select_all(action='DESELECT')
         self.sel_vert(neck_ind, mesh)
+        self.sel_vert(ear_ind, mesh)
         bpy.ops.mesh.delete(type='VERT')
         # TODO :delete ear
         # sel_vert(ear_ind, mesh)
