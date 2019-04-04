fbf70ae07943400f43e6f0a0b78c451bb3a19d3c
Ken Cheung <ykcheungab@connect.ust.hk>
2019-04-04 16:33:11 +0800

    added file name argument

--- import_hair.py
+++ import_hair.py
@@ -1,11 +1,11 @@
-def read_hair_obj(hair_path):
+def read_hair_obj(hair_path,save_name):
     f = open(hair_path,'r')
     hair = f.read().split('\n')
     hair_end = hair.index('o Face_Face-mesh')
 
-    with open('hair.obj','w') as fout:
+    with open(save_name,'w') as fout:
         for item in hair[:hair_end]:
             fout.write("%s\n" % item)
 
-read_hair_obj('./test.obj')
+read_hair_obj('./test.obj','out.obj')
     
