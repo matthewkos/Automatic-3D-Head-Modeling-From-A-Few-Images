e6bf353cefc88b81c62e7599f0840e943ae65e3f
David OR <36396488+orkapodavid@users.noreply.github.com>
2019-04-17 13:57:53 +0800

    David (#7)
    * Added function to convert mtl's and obj's file path to relative path
    
    * First Integration
    
    * fix bugs
    
    * fix bugs2
    
    * new texture scheme
    
    * doing texture
    
    * static tf sess
    
    * static sess
    
    * First Integration
    
    * fix bugs
    
    * fix bugs2
    
    * Luwdig (#6)
    
    * new texture scheme
    
    * doing texture
    
    * static tf sess
    
    * static sess
    
    * new_blender
    
    * new_blender
    
    * color scheme v4
    
    * new index files
    
    * temp
    
    * added new bledner
    
    * ok for align new_geometry.blender
    
    * Merge branch 'master' into ludwig
    
    # Conflicts:
    #	Data/mask/0.obj
    #	Data/mask/0_texture.png
    #	blender_script/geo.py
    #	main.py
    
    * merged sucess
    
    * Minor update on UI
    
    * UI Update: add more setting and preview
    
    * UI and Hairstyle Preview
    
    * add BLENDER_BACKGROUND config
    
    * Add OKP Image

--- ui.py
+++ ui.py
@@ -1,11 +1,16 @@
 import PySimpleGUI as sg
+import concurrent.futures
 import os
 import shutil
 import pyglet
+import warnings
+from time import time
 from datetime import datetime
 from PIL import Image
 from MeshViewer import MeshViewer
 from DataHelper import ConfigManager
+from main import *
+import tensorflow as tf
 
 class ImageViewer(object):
 
@@ -43,46 +48,84 @@ if __name__ == '__main__':
     UI_DISPLAY_HEIGHT = json_data["UI_DISPLAY_HEIGHT"]
     del json_data    
 
-    sg.ChangeLookAndFeel('DarkBlue')
+    #################### Setup #######################
+    
+    warnings.filterwarnings("ignore")
+    print("Importing packages: ")
+    start_time = time()
+    try:
+        from PRNet.myPRNET import genPRMask
+    except Exception as err:
+        print(err)
+        print("Cannot import PRNet. Please install all required packages in requirement.txt.")
+        print("pip install -r requirement.txt")
+    print("\ttime={:.2f}s".format(time() - start_time))
+
+    create tensorflow sess
+    sess = tf.Session(config=tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True)))
+    ##################################################
+
+    sg.ChangeLookAndFeel('Black')
     DEFAULT_INPUT = os.path.abspath(".\\" + DIR_INPUT + "\\" + INPUT_DATA)
     DEFAULT_INPUT_DISPLAY = os.path.abspath(".\\" + DIR_INPUT + "\\" + INPUT_DATA.replace('.jpg', '.png'))
     # TODO: Confirm the default INPUT and OUTPUT
     DEFAULT_OUTPUT = os.path.abspath(".\\" + DIR_OUT + "\\" + OUT_DATA)
+    DEFAULT_HEAD_OUTPUT = None
+    DEFAULT_HAIR_OUTPUT = None
     
+    # Menu Frame Layout
+    # Menu Definition
+    menu_bar_def = [['&File', ['&Open', '&Save', '---', 'Properties', 'E&xit'  ]],      
+                ['&Edit', ['Redo', 'Undo'],],      
+                ['&Help', '&About...'],]    
+
     # Input Frame Layout
     input_frame_layout = [
-         [sg.Text('Input Image Path:', size=(15, 1)), sg.Text(DEFAULT_INPUT, key='_IMG_PATH_DISPLAY_', size=(45, 1))],
-        [sg.Text('2D Frontal Image ', size=(15, 1)), sg.InputText(DEFAULT_INPUT, key='_IMG_PATH_'), sg.FileBrowse()],
-        [sg.Button('Preview')]
+        [sg.Text('Input Image Path:', size=(20, 1)), sg.Text(DEFAULT_INPUT, key='_IMG_PATH_DISPLAY_', size=(50, 1))],
+        [sg.Text('2D Frontal Image ', size=(20, 1)), sg.InputText(DEFAULT_INPUT, size=(50, 1), key='_IMG_PATH_'), sg.FileBrowse()],
+        [sg.Button('Preview'), sg.Button('Close', key='_close_preview_image_')]
+    ]
+
+    hairstyle_preview_frame_layout = [
+        [sg.Image(filename="Data\\ui_images\\strands00001.png" ,size=(UI_DISPLAY_WIDTH,UI_DISPLAY_HEIGHT), key='_HAIR_PREVIEW_1_', visible=True),],
+        [sg.Slider((1,50),  key='_HAIRSTYLE_PREVIEW_SLIDER_', orientation='h', enable_events=True, disable_number_display=False, size=(5,10), font=("Helvetica", 10))]
     ]
 
     # Image Frame Layout
     input_preview_frame_layout = [
-                  [sg.Image(filename=DEFAULT_INPUT_DISPLAY ,size=(UI_DISPLAY_WIDTH,UI_DISPLAY_HEIGHT), key='_IMAGE_PREVIEW_', visible=False)],      
-               ]  
+        [sg.Image(filename=DEFAULT_INPUT_DISPLAY ,size=(UI_DISPLAY_WIDTH,UI_DISPLAY_HEIGHT), key='_IMAGE_PREVIEW_', visible=True)],
+    ]  
 
+
+  
     # Generation Panel Frame Layout
     generation_panel_frame_layout =[
-        [sg.Radio('Full Model', group_id="Generation_Setting", key="_full_model_radio_", default=True), sg.Radio('Head Only', group_id="Generation_Setting", key="_head_only_radio_"), sg.Radio('Hair Only', group_id="Generation_Setting", key="_hair_only_radio_")],
+        [sg.Radio('Full Model', group_id="Generation_Setting", key="_full_model_radio_", default=True), ],
+        [sg.Radio('Head Only', group_id="Generation_Setting", key="_head_only_radio_", size=(10, 1)), ],
+        [sg.Radio('Hair Only', group_id="Generation_Setting", key="_hair_only_radio_", size=(10, 1)), ],
+        [sg.Text('Hair Model (.obj) File Path:', size=(20, 1)), sg.InputText(DEFAULT_HAIR_OUTPUT, size=(50, 1), key='_HAIR_OBJ_PATH_'), sg.FileBrowse()],
+        [sg.Text('Head Model (.obj) File Path:', size=(20, 1)), sg.InputText(DEFAULT_HEAD_OUTPUT, size=(50, 1), key='_HEAD_OBJ_PATH_'), sg.FileBrowse()],
+        [sg.Checkbox('Blender Background', key = "_blender_background_", default=True)],
         [sg.Button('Generate')]
     ]  
 
     model_preview_frame_layout = [
         [sg.Text('Output 3D Head .obj File Path:'), sg.Text('', key='_OBJ_PATH_DISPLAY_', size=(45, 1))],
-        [sg.Text('3D Head WaveFront ', size=(15, 1)), sg.InputText(DEFAULT_OUTPUT, key='_OBJ_PATH_'), sg.FileBrowse()],
+        [sg.Text('3D Head WaveFront ', size=(20, 1)), sg.InputText(DEFAULT_OUTPUT, size=(50, 1), key='_OBJ_PATH_'), sg.FileBrowse()],
         [sg.Button('Show 3D model')]
     ]
 
     # Main Layout 
     layout = [
-        [sg.Frame('Input', input_frame_layout, size=(15, 2), title_color='blue', key="_INPUT_FRAME_", visible=True)],
-        [sg.Frame('Input Preview', input_preview_frame_layout, size=(15, 2), title_color='blue', key="_IMG_PREVIEW_FRAME_", visible=False)],
-        [sg.Frame('Generation Control', generation_panel_frame_layout, size=(15, 2), title_color='blue', key="_GENERATION_CONTROL_FRAME_", visible=True)],
-        [sg.Frame('3D Model Preview', model_preview_frame_layout, size=(15, 2), title_color='blue', key="_MODEL_PREVIEW_FRAME_", visible=True)],
-        [sg.Button('Exit')]
-    ]
+        [sg.Menu(menu_bar_def)],
+        [sg.Frame('Input', input_frame_layout,title_color='white', key="_INPUT_FRAME_", visible=True),sg.Frame('Input Preview', input_preview_frame_layout,title_color='white', key="_IMG_PREVIEW_FRAME_", visible=True),],
+        [sg.Frame('Generation Control', generation_panel_frame_layout, title_color='white', key="_GENERATION_CONTROL_FRAME_", visible=True), sg.Frame('Hairstyle Preview', hairstyle_preview_frame_layout, title_color='white'),],
+        [sg.Frame('3D Model Preview', model_preview_frame_layout, title_color='white', key="_MODEL_PREVIEW_FRAME_", visible=True)],
 
-    window_main = sg.Window('Automatic Head Modelling').Layout(layout)
+    ]
+    # default window size = (698, 426)
+    window_main = sg.Window('Automatic Head Modelling',size=(850, 500), icon='Data\\ui_images\\icon.ico',auto_size_text=True, auto_size_buttons=True, resizable=True, grab_anywhere=False,).Layout(layout)
+    
     imageViewer_active = False
 
     current_img_path = ''
@@ -128,37 +171,87 @@ if __name__ == '__main__':
                     pass
                 print('display_img_path = ', display_img_path)
             
-                # Change the input image path in config.ini
+                # Update the input image path in config.ini
                 configManager.addOne('INPUT_DATA', input_data)
                 print('current_img_path = ', current_img_path)
 
                 window_main.FindElement('_IMG_PATH_').Update(current_img_path)
                 # Show another window for the 2D frontal image
                 window_main.FindElement('_IMG_PATH_DISPLAY_').Update(display_img_path)
-                # imageViewer = ImageViewer(filename_input=display_img_path)
-                # imageViewer.run()
-
                 window_main.FindElement('_IMAGE_PREVIEW_').Update(filename=display_img_path, size=(UI_DISPLAY_WIDTH,UI_DISPLAY_HEIGHT), visible=True)
                 window_main.FindElement('_IMG_PREVIEW_FRAME_').Update(visible=True)
             except Exception as err:
+                # Display the error by popup window
                 errmsg = str(err)
                 if len(errmsg) > 100:
                     errmsg = errmsg[:100] +"\n" + errmsg[100:]
                 sg.PopupError(errmsg)
+        elif event == '_close_preview_image_':
+            window_main.FindElement('_IMG_PREVIEW_FRAME_').Update(visible=False)
+
+        elif event == '_HAIRSTYLE_PREVIEW_SLIDER_':
+            # Slide to change hairstyle preview images
+            slider_value = values['_HAIRSTYLE_PREVIEW_SLIDER_']
+            window_main.FindElement('_HAIR_PREVIEW_1_').Update('Data\\ui_images\\strands00002.png',size=(UI_DISPLAY_WIDTH,UI_DISPLAY_HEIGHT))
         elif event == 'Generate':
             if current_img_path == "":
                 # use default image if user does not input a path before
-                pass
+                print("Empty image path input.")
+                print("Use default image path:", DEFAULT_INPUT)
+                current_img_path = DEFAULT_INPUT
+
+            # Change to relative path
+            relative_current_img_path = os.path.relpath(current_img_path, os.getcwd())
+            assert os.path.exists(relative_current_img_path), "Invalid path: "+relative_current_img_path
+            print("Relative image path:", relative_current_img_path)
+
+            # Update the BLENDER_BACKGROUND setting in config.ini
+            configManager.addOne('BLENDER_BACKGROUND', values["_blender_background_"])
+
             if values["_full_model_radio_"]:
                 # Generate complete model
+                global_start = time()
+                """Geometry"""
+                time_it_wrapper(None, "Generating Geometry")
+                """Mask"""
+                time_it_wrapper(genPRMask, "Generating Mask", (relative_current_img_path, DIR_MASK),
+                                kwargs={'isMask': False})
+                """Texture"""
+                time_it_wrapper(genText, "Generating Texture", (
+                    os.path.join(DIR_MASK, "{}_texture.png".format(MASK_DATA[:-4])),
+                    os.path.join(DIR_TEXTURE, TEXTURE_DATA)))
+                """Alignment"""
+                time_it_wrapper(blender_wrapper, "Alignment",
+                                args=(
+                                    ".\\geometry.blend", ".\\blender_script\\geo.py", INPUT_DATA, TEXTURE_DATA,
+                                    HAIR_DATA,
+                                    MASK_DATA, OUT_DATA, True, False))
+                print("Output to: {}".format(os.path.join(os.getcwd(), DIR_OUT, OUT_DATA)))
+                print("Total_time: {:.2f}".format(time() - global_start))
                 pass
             elif values["_head_only_radio_"]:
                 # Generate head model only
+                global_start = time()
+                """Geometry"""
+                time_it_wrapper(None, "Generating Geometry")
+                """Mask"""
+                time_it_wrapper(genPRMask, "Generating Mask", (relative_current_img_path, DIR_MASK))
+                """Texture"""
+                time_it_wrapper(genText, "Generating Texture", (
+                    os.path.join(DIR_MASK, "{}_texture.png".format(MASK_DATA[:-4])),
+                    os.path.join(DIR_TEXTURE, TEXTURE_DATA)))
+                """Alignment"""
+                time_it_wrapper(blender_wrapper, "Alignment",
+                                args=(
+                                    ".\\geometry.blend", ".\\blender_script\\geo.py", INPUT_DATA, TEXTURE_DATA,
+                                    HAIR_DATA,
+                                    MASK_DATA, OUT_DATA, False, False))
+                print("Output to: {}".format(os.path.join(os.getcwd(), DIR_OUT, OUT_DATA)))
+                print("Total_time: {:.2f}".format(time() - global_start))
                 pass
             elif values["_hair_only_radio_"]:
                 # Generate hair model only
                 pass
-            # main.gen(DEFAULT_INPUT)
 
             # After Generation:
             current_obj_path = ".\\" + DIR_OUT + "\\" + configManager.getOne("OUT_DATA")
